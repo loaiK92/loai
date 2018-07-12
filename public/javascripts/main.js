ce64c2285e34b0e829ed63a9e9d2add8d23f7562
loaikahel1992 <loaikahel723@gmail.com>
2018-07-12 17:10:15 +0200

    voiceRec works good

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,7 @@ import "../sass/style.scss";
 import SweetScroll from "sweet-scroll";
 import contactFrom from "./mudoles/contactForm";
 import all from "./mudoles/animation";
-// import submitForm from "./mudoles/submitForm";
+import submitForm from "./mudoles/submitForm";
 const MenuSpy = require("menuspy");
 
 // control scroll event in the page from SweetScroll
@@ -89,8 +89,19 @@ if (annyang) {
       tag = tag.replace(" at ", "@");
       tag = tag.replace(/\s/g, "");
       element.value = tag;
-
       console.log("mail: " + tag);
+    },
+    "message *tag": function(tag) {
+      let element = document.querySelector('#msg')
+      element.focus();
+      element.value = tag;
+      console.log("message: " + tag);
+    },
+    "submit": function() {
+      const form = document.querySelector(".contact__form");
+      form.addEventListener("submit", submitForm);
+      form.submit();
+      console.log("submitted message");
     }
   };
 
