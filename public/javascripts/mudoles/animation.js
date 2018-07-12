16fe5bbef005c661e812e5e8d1fde3046bff4a4a
loaikahel1992 <loaikahel723@gmail.com>
2018-07-12 14:00:16 +0200

    adding again

--- public/javascripts/mudoles/animation.js
+++ public/javascripts/mudoles/animation.js
@@ -1,121 +1,123 @@
-$(document).ready(function(){   
+$(document).ready(function() {
+  // set the height of frontend--border && backend--border
+  $(".frontend--border").css({
+    height: `${$(".frontend--content").height() + 35}px`
+  });
+  $(".backend--border").css({
+    height: `${$(".backend--content").height() + 35}px`
+  });
 
-    // set the height of frontend--border && backend--border
-    $('.frontend--border').css({'height': `${$('.frontend--content').height() + 35}px`});
-    $('.backend--border').css({'height': `${$('.backend--content').height() + 35}px`});
+  // page elements animation ScrollReveal library
+  window.sr = ScrollReveal();
+  sr.reveal(".nav__left", {
+    origin: "left",
+    distance: "500px",
+    delay: 500,
+    duration: 500,
+    easing: "cubic-bezier(.61,.19,.44,1.37)"
+  });
+  sr.reveal(".nav__right", {
+    origin: "right",
+    distance: "500px",
+    delay: 500,
+    duration: 500,
+    easing: "cubic-bezier(.61,.19,.44,1.37)"
+  });
+  sr.reveal(".intro__home--content", {
+    origin: "bottom",
+    distance: "100px",
+    delay: 1200,
+    duration: 300,
+    easing: "cubic-bezier(.44,.44,.75,1.18)"
+  });
+  sr.reveal(".intro__about", {
+    origin: "left",
+    distance: "500px",
+    delay: 700,
+    duration: 500,
+    easing: "cubic-bezier(.44,.44,.75,1.18)"
+  });
+  sr.reveal(".intro__skills", {
+    origin: "left",
+    distance: "40px",
+    delay: 1300,
+    duration: 1000,
+    easing: "cubic-bezier(.44,.44,.75,1.18)"
+  });
 
-    // page elements animation ScrollReveal library
-    window.sr = ScrollReveal();
-    sr.reveal('.nav__left', { 
-        origin: 'left', 
-        distance: '500px',
-        delay: 500,
-        duration: 500,
-        easing: 'cubic-bezier(.61,.19,.44,1.37)'
-    });
-    sr.reveal('.nav__right', { 
-        origin: 'right', 
-        distance: '500px',
-        delay: 500,
-        duration: 500,
-        easing: 'cubic-bezier(.61,.19,.44,1.37)'
-    });
-    sr.reveal('.intro__home--content', { 
-        origin: 'bottom', 
-        distance: '100px',
-        delay: 1200,
-        duration: 300,
-        easing: 'cubic-bezier(.44,.44,.75,1.18)'
-    });
-    sr.reveal('.intro__about', { 
-        origin: 'left', 
-        distance: '500px',
-        delay: 700,
-        duration: 500,
-        easing: 'cubic-bezier(.44,.44,.75,1.18)'
-    });
-    sr.reveal('.intro__skills', { 
-        origin: 'left', 
-        distance: '40px',
-        delay: 1300,
-        duration: 1000,
-        easing: 'cubic-bezier(.44,.44,.75,1.18)'
-    });
+  sr.reveal(".frontend--border", {
+    origin: "right",
+    distance: "20px",
+    delay: 1600,
+    duration: 1000,
+    easing: "cubic-bezier(.44,.44,.75,1.18)"
+  });
 
-    sr.reveal('.frontend--border', { 
-        origin: 'right', 
-        distance: '20px',
-        delay: 1600,
-        duration: 1000,
-        easing: 'cubic-bezier(.44,.44,.75,1.18)'
-    });
+  sr.reveal(".backend--border", {
+    origin: "right",
+    distance: "20px",
+    delay: 1600,
+    duration: 1000,
+    easing: "cubic-bezier(.44,.44,.75,1.18)"
+  });
 
-    sr.reveal('.backend--border', { 
-        origin: 'right', 
-        distance: '20px',
-        delay: 1600,
-        duration: 1000,
-        easing: 'cubic-bezier(.44,.44,.75,1.18)'
-    });
+  sr.reveal(".skills--content--frontend", {
+    origin: "right",
+    distance: "40px",
+    delay: 1700,
+    duration: 500,
+    easing: "cubic-bezier(.61,.19,.44,1.37)"
+  });
 
-    sr.reveal('.skills--content--frontend', {
-        origin: 'right', 
-        distance: '40px',
-        delay: 1700,
-        duration: 500,
-        easing: 'cubic-bezier(.61,.19,.44,1.37)'
-    });
+  sr.reveal(".skills--border--frontend", {
+    origin: "right",
+    distance: "200px",
+    delay: 1300,
+    duration: 500,
+    easing: "cubic-bezier(.44,.44,.75,1.18)",
+    opacity: 0
+  });
 
-    sr.reveal('.skills--border--frontend', {
-        origin: 'right', 
-        distance: '200px',
-        delay: 1300,
-        duration: 500,
-        easing: 'cubic-bezier(.44,.44,.75,1.18)',
-        opacity: 0
-    });
+  sr.reveal(".skills--content--tools", {
+    origin: "right",
+    distance: "40px",
+    delay: 1000,
+    duration: 500,
+    easing: "cubic-bezier(.61,.19,.44,1.37)"
+  });
 
-    sr.reveal('.skills--content--tools', {
-        origin: 'right', 
-        distance: '40px',
-        delay: 1000,
-        duration: 500,
-        easing: 'cubic-bezier(.61,.19,.44,1.37)'
-    });
+  sr.reveal(".skills--border--tools", {
+    origin: "right",
+    distance: "200px",
+    delay: 700,
+    duration: 500,
+    easing: "cubic-bezier(.44,.44,.75,1.18)",
+    opacity: 0
+  });
 
-    sr.reveal('.skills--border--tools', {
-        origin: 'right', 
-        distance: '200px',
-        delay: 700,
-        duration: 500,
-        easing: 'cubic-bezier(.44,.44,.75,1.18)',
-        opacity: 0
-    });
+  sr.reveal(".skills--content--backend", {
+    origin: "right",
+    distance: "40px",
+    delay: 300,
+    duration: 500,
+    easing: "cubic-bezier(.61,.19,.44,1.37)"
+  });
 
-    sr.reveal('.skills--content--backend', {
-        origin: 'right', 
-        distance: '40px',
-        delay: 300,
-        duration: 500,
-        easing: 'cubic-bezier(.61,.19,.44,1.37)'
-    });
+  sr.reveal(".skills--border--backend", {
+    origin: "right",
+    distance: "200px",
+    delay: 0,
+    duration: 500,
+    easing: "cubic-bezier(.44,.44,.75,1.18)",
+    opacity: 0
+  });
 
-    sr.reveal('.skills--border--backend', {
-        origin: 'right', 
-        distance: '200px',
-        delay: 0,
-        duration: 500,
-        easing: 'cubic-bezier(.44,.44,.75,1.18)',
-        opacity: 0
-    });
-
-    // set intro__qoutes slider from slick-sliders   
-    $(".intro__qoutes--items").slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        adaptiveHeight: true
-    });
-    
+  // set intro__qoutes slider from slick-sliders
+  $(".intro__qoutes--items").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    adaptiveHeight: true
+  });
 });
 export default document;
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const javascript = {
   test: /\.(js)$/,
-  use: [{
-      loader: 'babel-loader',
-      options: { presets: ['env'] }
-  }],
+  use: [
+    {
+      loader: "babel-loader",
+      options: { presets: ["env"] }
+    }
+  ]
 };
 
 const postcss = {
-  loader: 'postcss-loader',
+  loader: "postcss-loader",
   options: {
-    plugins() { return [autoprefixer({ browsers: 'last 6 versions' })]; }
+    plugins() {
+      return [autoprefixer({ browsers: "last 6 versions" })];
+    }
   }
 };
-
 
 const styles = {
   test: /\.scss$/,
   use: ExtractTextPlugin.extract({
-    fallback: 'style-loader',
-    use: [{loader: 'css-loader', options: { minimize: true } },postcss, 'sass-loader']
+    fallback: "style-loader",
+    use: [
+      { loader: "css-loader", options: { minimize: false } },
+      postcss,
+      "sass-loader"
+    ]
   })
 };
 
@@ -33,29 +39,26 @@ const images = {
   test: /\.(jpg|png|jpeg|svg)$/,
   use: [
     {
-      loader: 'file-loader',
+      loader: "file-loader",
       options: {
-        name: '[name].[ext]',
-        outputPath: 'images/',
-        publicPath: 'images/'
+        name: "[name].[ext]",
+        outputPath: "images/",
+        publicPath: "images/"
       }
     }
   ]
-}
+};
 
 module.exports = {
-  mode: 'production',
-  entry: './public/javascripts/main.js',
+  mode: "production",
+  entry: "./public/javascripts/main.js",
   output: {
-    path: path.resolve(__dirname, 'public', 'dist'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "public", "dist"),
+    filename: "[name].bundle.js"
   },
   module: {
     rules: [javascript, styles, images]
   },
-  plugins: [
-    new ExtractTextPlugin('main.css'),
-    new UglifyJsPlugin()
-  ]
+  plugins: [new ExtractTextPlugin("main.css"), new UglifyJsPlugin()]
 };
 process.noDeprecation = true;
